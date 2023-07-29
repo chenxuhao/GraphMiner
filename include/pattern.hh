de757fe7f399081aee36c3dd8a469cfdf5960bbf
Xuhao Chen <cxh@mit.edu>
2023-07-29 09:36:11 -0400

    fix a counting bug

--- include/pattern.hh
+++ include/pattern.hh
@@ -44,7 +44,8 @@ private:
 
 public:
   Pattern() : Pattern("", false) { }
-  Pattern(std::string name) : name_(name) { }
+  Pattern(std::string name) : Pattern(name, 0, 0) { }
+  Pattern(std::string name, int nv, int ne) : name_(name), num_vertices(nv), num_edges(ne) { }
   Pattern(std::string filename, bool is_labeled) : 
       name_(""), has_label(is_labeled), core_length_(0) {
     read_adj_file(filename);
@@ -52,7 +53,7 @@ public:
     set_name();
   }
   ~Pattern() {}
-  bool is_clique() const { return num_edges == (num_vertices-1)*num_vertices/2; }
+  bool is_clique() const { return num_vertices>1 && num_edges == (num_vertices-1)*num_vertices/2; }
   bool is_path() const { return num_edges == num_vertices-1; }
   bool is_chain() const { return num_edges == num_vertices-1; }
   bool is_wedge() const { return name_ == "wedge"; }
