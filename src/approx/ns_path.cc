439a7437941e0f2ddc43d2250bba7b62b920461e
Xuhao Chen <cxh@mit.edu>
2023-08-02 09:50:37 -0400

    fix ns_path bug

--- src/approx/ns_path.cc
+++ src/approx/ns_path.cc
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <random>
 #include "graph.h"
+#include <iomanip>
+#include <locale>
 
-vidType sample_neighbor(Graph &g, vidType v, VertexSet &vs, vidType &num) {
-  auto candidate_set = difference_set(g.N(v), vs);
+template<class T>
+std::string FormatWithCommas(T value) {
+  std::stringstream ss;
+  ss.imbue(std::locale(""));
+  ss << std::fixed << value;
+  return ss.str();
+}
+
+vidType sample_neighbor(Graph &g, vidType v, VertexSet &vs, vidType &num, vidType upper_bound = INT_MAX) {
+  VertexSet candidate_set;
+  if (upper_bound < INT_MAX) 
+    difference_set(candidate_set, g.N(v), vs, upper_bound);
+  else
+    difference_set(candidate_set, g.N(v), vs);
   num = candidate_set.size();
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -28,22 +42,37 @@ int main(int argc, char* argv[]) {
   auto m = g.E();
   Timer t;
   t.Start();
-  uint64_t counter = 0;
+  __int128_t counter = 0;
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<eidType> dist(0, m);
-  //#pragma omp parallel for reduction(+ : counter) schedule(dynamic, 1)
+  std::vector<eidType> edges(num_samples);
+  for (eidType i = 0; i < num_samples; i++)
+    edges[i] = dist(gen);
+
+  int num_threads = 1;
+  #pragma omp parallel
+  {
+    num_threads = omp_get_num_threads();
+  }
+  std::cout << "OpenMP (" << num_threads << " threads)\n";
+ 
+  #pragma omp parallel for reduction(+ : counter) //schedule(dynamic, 1)
   for (eidType i = 0; i < num_samples; i++) {
-    eidType scale = m;
-    auto eid = dist(gen);
+    uint64_t scale = m;
+    auto eid = edges[i];
     auto v0 = g.get_src(eid);
     auto v1 = g.get_dst(eid);
     VertexSet vs;
     vs.add(v0);
     vs.add(v1);
+    vidType v;
     std::vector<vidType> c(path_length-1);
     for (int j = 1; j < path_length; j++) {
-      auto v = sample_neighbor(g, vs[j], vs, c[j-1]);
+      if (j == path_length-1)
+        v = sample_neighbor(g, vs[j], vs, c[j-1], v0);
+      else
+        v = sample_neighbor(g, vs[j], vs, c[j-1]);
       if (c[j-1] == 0) {
         scale = 0;
         break;
@@ -55,8 +84,7 @@ int main(int argc, char* argv[]) {
   }
   // scale down by number of samples
   uint64_t total = counter / num_samples;
-  total /= 2; // symmetry breaking
   t.Stop();
   std::cout << "runtime = " << t.Seconds() << " sec\n";
-  printf("estimated count: %lu\n", total);
+  std::cout << "Estimated count " << FormatWithCommas(total) << "\n";
 }
